4daa34f0a0e113d5920542a1c32009da3efcf071
santhiyaajith <43902533+santhiyaajith@users.noreply.github.com>
2019-02-12 10:53:49 +0530

    Add files via upload

---
+++ vowels.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class vowels{
+public static void main(String args[])
+{
+
+char a=in.next().charAt(0);
+if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U')
+{
+System.out.println("vowel");
+    
+}
+else
+{
+System.out.println("consonant");}}}
